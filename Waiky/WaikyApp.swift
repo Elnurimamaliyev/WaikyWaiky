be093d576e7a21ea91fa292628138b6e37a29835
Elnur Imamaliyev <imamaliyevelnur@gmail.com>
2026-08-01 12:34:45 +0200

    Initial Commit

---
+++ Waiky/WaikyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaikyApp.swift
+//  Waiky
+//
+//  Created by Elnur Imamaliyev on 01.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct WaikyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
